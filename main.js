f26a4a325388281867cd70fda71e3eee467188e3
Theozra <148161578+Theozra@users.noreply.github.com>
2023-10-16 19:55:13 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function Change_Picture(){
+  document.getElementById("Peralta-Normal-outfit").src="https://i.postimg.cc/3wmhDcj0/Peralta-formal-dress.jpg";
+  
+}
+function ChangeThroughMouseOver(){ document.getElementById("IMAGEEE!").src="https://i.postimg.cc/Cx0g7mqt/JP-Halloween.jpg";
+}
+function ImageBackToNormal(){ document.getElementById("IMAGEEE!").src="https://i.postimg.cc/FzkJM4dy/JP.jpg";
+}
